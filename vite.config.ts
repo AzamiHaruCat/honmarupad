1de744e7282e2c98868c031ba488a87da2397d70
AzamiHaruCat <haruazamicat+git@gmail.com>
2026-01-27 15:41:04 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -25,11 +25,11 @@ export default defineConfig(({ command }) => {
             'ブラウザ版「刀剣乱舞ONLINE」のゲーム画面を調整するユーザースクリプトです。このスクリプトはブラウザの表示（DOM）のみを調整します。ゲームデータや通信内容には一切触れていません。ブラウザの標準機能で実現できる表示制御をスクリプトで代行しているだけですが、各位の判断で自己責任のもとご利用ください。',
           license: 'Custom (See LICENSE file)',
           icon: 'https://sanipad.pages.dev/favicon.svg',
-          // downloadURL:
-          //   'https://raw.githubusercontent.com/ユーザー名/リポジトリ名/main/dist/honmarupad.user.js',
-          // updateURL:
-          //   'https://raw.githubusercontent.com/ユーザー名/リポジトリ名/main/dist/honmarupad.user.js',
-          // supportURL: 'https://github.com/ユーザー名/リポジトリ名/issues',
+          downloadURL:
+            'https://raw.githubusercontent.com/AzamiHaruCat/honmarupad/main/dist/honmarupad.user.js',
+          updateURL:
+            'https://raw.githubusercontent.com/AzamiHaruCat/honmarupad/main/dist/honmarupad.user.js',
+          supportURL: 'https://github.com/AzamiHaruCat/honmarupad/issues',
           homepageURL: 'https://sanipad.pages.dev/',
           author: 'AzamiHaru',
           match: matchPatterns,
